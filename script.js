d1eea59d98f674079390a15955b4d01f91371d37
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-08-16 16:59:37 -0400

    Add the GameController object to control the flow of the game

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const Player = function (name, mark){
     return {getName, getMark};
 };
 
-const gameBoard = (function GameBoard(){
+function GameBoard(){
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -22,6 +22,10 @@ const gameBoard = (function GameBoard(){
             board[i].push(Cell());
         }
     }
+
+    const getRows = () => rows;
+    const getColumns = () => columns;
+    const getBoard = () => board;
 
     const displayBoard = () => {
         for(let i = 0; i < rows; i++){
@@ -38,5 +42,126 @@ const gameBoard = (function GameBoard(){
         board[row][column].setValue(playerMark);
     };
 
-    return {displayBoard, placeMark};
-})();
+    return {displayBoard, placeMark, getRows, getColumns, getBoard};
+};
+
+function GameController(gameBoard){
+    const players = [Player("player1", "X"), Player("player2", "O")];
+
+    let activePlayer = players[0];
+
+    const switchActivePlayer = () => activePlayer = activePlayer === players[0] ? 
+    players[1] : players[0];
+
+    const welcomePlayers = () => {
+        console.log("Welcome to Tic Tac Toe! The game will begin immediately.");
+        console.log("*note that 'E' denotes an empty cell to place your mark");
+    };
+
+    const displayCongratsMessage = () => {
+        console.log(`Congratulations to ${activePlayer.getName()} for winning!`);
+    };
+
+    const displayTieMessage = () => console.log("Nobody won... there was a tie!");
+
+    const checkThreeInARow = (mark) => {
+        let numberOfMarksInSuccession = 0;
+        
+        for(let i = 0; i < gameBoard.getRows(); i++){
+            for(let k = 0; k < gameBoard.getColumns(); k++){
+                if(gameBoard.getBoard()[i][k].getValue() === mark) numberOfMarksInSuccession++;
+            }
+
+            if(numberOfMarksInSuccession === gameBoard.getColumns()){
+                return true;
+            }else {
+                numberOfMarksInSuccession = 0;
+            }
+        }
+        return false;
+    }
+
+    const checkThreeInAColumn = (mark) => {
+        let numberOfMarksInSuccession = 0;
+        
+        for(let i = 0; i < gameBoard.getColumns(); i++){
+            for(let k = 0; k < gameBoard.getRows(); k++){
+                if(gameBoard.getBoard()[k][i].getValue() === mark) numberOfMarksInSuccession++;
+            }
+
+            if(numberOfMarksInSuccession === gameBoard.getRows()){
+                return true;
+            }else {
+                numberOfMarksInSuccession = 0;
+            }
+        }
+        return false;
+    };
+
+    const hasLeftToRightDiagonal = (board, mark) => {
+        return board[0][0].getValue() === mark &&
+        board[1][1].getValue() === mark &&
+        board[2][2].getValue() === mark;
+    };
+
+    const hasRightToLeftDiagonal = (board, mark) => {
+        return board[0][2].getValue() === mark &&
+        board[1][1].getValue() === mark &&
+        board[2][0].getValue() === mark;
+    };
+
+    const checkThreeInADiagonal = (mark) => {
+        const copyOfBoard = gameBoard.getBoard();
+        return hasLeftToRightDiagonal(copyOfBoard, mark) || hasRightToLeftDiagonal(copyOfBoard, mark); 
+    };
+
+    const checkIfActivePlayerWon = (activePlayer) => {
+        const mark = activePlayer.getMark();
+        return checkThreeInARow(mark) || checkThreeInAColumn(mark) || checkThreeInADiagonal(mark);
+    };
+
+    const isFilled = () => {
+        const boardArray = gameBoard.getBoard();
+        for(let i = 0; i < gameBoard.getRows(); i++){
+            for(let k = 0; k < gameBoard.getColumns(); k++){
+                if(boardArray[i][k].getValue() === "E") return false;
+            }
+        }
+        return true;
+    };
+
+    const checkForATie = (activePlayer) => !checkIfActivePlayerWon(activePlayer) && isFilled();
+
+    const getMarkLocationFor = (dimensionOfBoard) => {
+        return prompt(`${activePlayer.getName()}, which ${dimensionOfBoard} will you place your mark?`, "1");
+    };
+
+    const playTurn = () => {
+        gameBoard.displayBoard();
+        let userRow = getMarkLocationFor("row");
+        let userColumn = getMarkLocationFor("column");
+        gameBoard.placeMark(userRow, userColumn, activePlayer.getMark());
+    };
+
+    const playFullGame = () => {
+        welcomePlayers();
+        playTurn();
+        console.log("");
+        while(!checkIfActivePlayerWon(activePlayer) && !checkForATie(activePlayer)){
+            switchActivePlayer();
+            playTurn();
+            console.log("");
+        }
+        if(checkIfActivePlayerWon(activePlayer)){
+            gameBoard.displayBoard();
+            displayCongratsMessage();
+            return;
+        }
+        gameBoard.displayBoard();
+        displayTieMessage();
+    };
+
+    return {playFullGame};
+};
+
+GameController(GameBoard()).playFullGame();
